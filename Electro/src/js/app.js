5898c39e9551e1267bfcf6eda8a2e542ac990818
zaurkhalilli <ggencetel274@gmail.com>
2023-04-01 18:32:01 +0400

    slider added

--- Electro/src/js/app.js
+++ Electro/src/js/app.js
@@ -8,3 +8,31 @@ dropdownBtn.addEventListener('click', function() {
     dropdownMenu.style.display = 'none';
   }
 });
+
+
+const dropdown = document.querySelector('.dropdown-hover');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+dropdown.addEventListener('mouseover', () => {
+  dropdownContent.style.display = 'block';
+});
+
+dropdown.addEventListener('mouseout', () => {
+  dropdownContent.style.display = 'none';
+});
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
