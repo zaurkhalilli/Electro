1aafbb11b16f0f20789ca8cc0528eaf356572e0a
zaurkhalilli <ggencetel274@gmail.com>
2023-04-08 22:46:25 +0400

    new adds

--- Electro/src/js/app.js
+++ Electro/src/js/app.js
@@ -84,3 +84,14 @@ var swiper = new Swiper(".mySwiper-", {
       swiper: swiper,
     },
   });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
